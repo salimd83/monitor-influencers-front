c222baf5ea08f7c3cd446a5187e47e047889553d
Salim Dirani <salimdirani@gmail.com>
2018-08-06 16:52:21 +0300

    SHI-155-tags-filter-media

--- src/main/content/apps/media/filters/TagsFilter.jsx
+++ src/main/content/apps/media/filters/TagsFilter.jsx
@@ -6,21 +6,28 @@ import * as Fn from "fn/simpleCall.js";
 
 class TagsFilter extends Component {
   getTagsOptions = (input, callback) => {
+    const req1 = Fn.simpleCall("get", `typeahead/simedia_tag?q=${input}`);
+    const req2 = Fn.simpleCall("get", `typeahead/media_label?q=${input}`);
+    const req3 = Fn.simpleCall("get", `typeahead/location?q=${input}`);
+    const req4 = Fn.simpleCall("get", `typeahead/brand?q=${input}`);
 
-    const request = Fn.simpleCall("get", `typeahead/simedia_tag?q=${input}`);
-
-    request.then(response => {
-
-      callback(null, {
-        options: response.data.map(tag => ({
-          label: tag.name,
-          value: tag.name,
-          id: tag.id
-        })),
-        complete: true
-      });
-    }).catch(e => console.log(e));
-  
+    Promise.all([req1, req2, req3, req4])
+      .then(response => {
+        
+        const data = response.map(res => res.data)
+        console.log('data', data)
+        const flatData = [].concat(...data)
+        console.log('flat',flatData)
+        callback(null, {
+          options: flatData.map(tag => ({
+            label: tag.name,
+            value: tag.name,
+            id: tag.id
+          })),
+          complete: true
+        });
+      })
+      .catch(e => console.log(e));
   };
 
   render() {
