832d88bdb6529174172051de06cb9b246e91f965
Salim Dirani <salimdirani@gmail.com>
2018-09-27 10:03:01 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
 // app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(express.static(`${__dirname}/build`));
@@ -8,5 +9,6 @@ app.use(express.static(`${__dirname}/build`));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/../build/index.html"));
 });
+
 
 app.listen(process.env.PORT || 8080);
